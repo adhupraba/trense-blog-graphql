7de5842286043cc5f872491155ef8baf494490cb
adharsh <adharsh@codingmart.com>
2022-02-12 19:32:00 +0530

    added proxy and update httplink uri in client side

--- client/src/Apollo.js
+++ client/src/Apollo.js
@@ -1,32 +1,32 @@
-import React from 'react'
-import { ApolloProvider, ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
-import App from './App'
-import { setContext } from '@apollo/client/link/context'
+import React from "react";
+import { ApolloProvider, ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+import App from "./App";
+import { setContext } from "@apollo/client/link/context";
 
 const httpLink = new HttpLink({
-    uri: 'https://trense-blog.herokuapp.com/graphql'
-})
+    uri: "/graphql",
+});
 
 const authLink = setContext((operation, prevContext) => {
-    const token = localStorage.getItem('token')
+    const token = localStorage.getItem("token");
     return {
         headers: {
-            Authorization: token ? `Bearer ${token}` : null
-        }
-    }
-})
+            Authorization: token ? `Bearer ${token}` : null,
+        },
+    };
+});
 
 const client = new ApolloClient({
     link: authLink.concat(httpLink),
-    cache: new InMemoryCache()
-})
+    cache: new InMemoryCache(),
+});
 
 const Apollo = () => {
     return (
-        <ApolloProvider client={client} >
+        <ApolloProvider client={client}>
             <App />
         </ApolloProvider>
-    )
-}
+    );
+};
 
-export default Apollo
+export default Apollo;
